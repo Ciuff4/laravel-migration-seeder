52308bd086591ba1a0920fc8bc8a223e620a08a1
Ciuff4 <gasperini_cristina@libero.it>
2022-06-27 17:59:59 +0200

    reazione db trains+ base tabella

--- database/migrations/2022_06_27_145916_create_trains_table.php
+++ database/migrations/2022_06_27_145916_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 200);
+            $table->string('start_station', 200);
+            $table->string('end_station', 200);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->char('train_number');
+            $table->smallInteger('coaches_number');
+            $table->boolean('is_onTime');
+            $table->boolean('is_Cancelled');
             $table->timestamps();
         });
     }
